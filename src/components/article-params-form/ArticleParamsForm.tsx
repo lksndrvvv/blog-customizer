953f793da6622fdc7da78d590bce6d9b47bd0402
Maria Aleksandrova <alexandrova.mk@yandex.ru>
2026-06-24 14:43:31 +0300

    feat: implement toggle functionality for article parameters form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,45 @@
+import { useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const formRef = useRef<HTMLDivElement>(null);
+
+	const handleToggleForm = () => {
+		setIsOpen((currentIsOpen) => !currentIsOpen);
+	};
+
+	useEffect(() => {
+		if (!isOpen) {
+			return;
+		}
+
+		const handleOutsideClick = (event: MouseEvent) => {
+			if (formRef.current && !formRef.current.contains(event.target as Node)) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('click', handleOutsideClick);
+
+		return () => {
+			document.removeEventListener('click', handleOutsideClick);
+		};
+	}, [isOpen]);
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+		<div ref={formRef}>
+			<ArrowButton isOpen={isOpen} onClick={handleToggleForm} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
@@ -15,6 +47,6 @@ export const ArticleParamsForm = () => {
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
